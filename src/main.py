cae02a76f1b9e16e291f4b115f0a2cb91d83e48d
Dragos Crintea <hikari.labs@icloud.com>
2025-12-27 16:37:33 +0000

    ch5: website - github pages

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ import sys
 from copy_static import copy_files_recursive
 from generate_content import generate_pages_recursive
 
+
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 default_basepath = "/"
-
 
 
 def main():
